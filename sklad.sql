0607788922a181a15b6d2ce385596ea54282ef89
edumach <59918768+edumach@users.noreply.github.com>
2024-12-29 20:15:11 +0100

    Update sklad.sql

--- sklad.sql
+++ sklad.sql
@@ -1,3 +1,12 @@
+/* Založení databáze eshop */
+CREATE DATABASE IF NOT EXISTS eshop;
+
+
+/* nastavení aktivní databáze */
+USE eshop;
+
+
+/* Vytvoření tabulky sklad */
 CREATE TABLE sklad (
   id int PRIMARY KEY AUTO_INCREMENT,
   kategorie VARCHAR(30),
@@ -6,9 +15,12 @@ CREATE TABLE sklad (
   pocet int unsigned NOT NULL DEFAULT 0,
 );
 
+
+/* Dva záznamy */
 INSERT INTO sklad (kategorie, nazev, cena, pocet) VALUES ("Tričko", "Tričko modré", 200, 6);
 INSERT INTO sklad (kategorie, nazev, cena, pocet) VALUES ("Tričko", "Tričko zelené", 250, 10);
 
+/* Další záznamy v jednom INSERT */
 INSERT INTO sklad (kategorie, nazev, cena, pocet) VALUES
 ("Deštník", "Deštník velký", 300, 5),
 ("Deštník", "Deštník malý", 150, 2),
